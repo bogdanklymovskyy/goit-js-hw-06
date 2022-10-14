391f02f9e38f79dc17a5de3bfbd429702d0a85ff
bogdanklymovskyy <bogdanklymovskyy@gmail.com>
2022-10-14 14:08:33 +0200

    update

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const link = document.querySelector('.change-color');
+const body = document.querySelector('body')
+const span = document.querySelector('.color');
+
+
+let color = getRandomHexColor()
+
+
+link.addEventListener("click", () => {
+body.style.backgroundColor = color
+span.textContent = color
+});
